933ac52de5dfd1e9e007c088afd69e0919c04379
thientn <thientn@midota.com>
2023-02-17 10:29:02 +0700

    feat: 2

--- src/components/Form/SimpleForm.vue
+++ src/components/Form/SimpleForm.vue
@@ -1,56 +1,62 @@
-<template lang="">
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
+<template>
+  <div>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name">
   </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
 </template>
-<script>
-export default {
-  name: 'SimpleForm'
-}
-</script>
-<style lang="">
 
-</style>
+<script>
+import { openDB } from 'idb';
+
+const dbPromise = openDB('my-db', 1, {
+  upgrade(db) {
+    db.createObjectStore('inputs');
+  }
+});
+
+export default {
+  data() {
+    return {
+      name: ''
+    };
+  },
+  mounted() {
+    dbPromise.then(db => {
+      return db.get('inputs', 'name');
+    }).then(value => {
+      if (value) {
+        this.name = value;
+      }
+    });
+  },
+  watch: {
+    name(newValue) {
+      dbPromise.then(db => {
+        return db.put('inputs', newValue, 'name');
+      });
+    }
+  },
+  created() {
+    window.addEventListener('offline', () => {
+      // Set a flag in IndexedDB indicating the app is offline
+      dbPromise.then(db => {
+        return db.put('inputs', 'true', 'offline');
+      });
+    });
+    window.addEventListener('online', () => {
+      // Remove the flag from IndexedDB when the app is online again
+      dbPromise.then(db => {
+        return db.delete('inputs', 'offline');
+      });
+      // Update the input values from IndexedDB
+      dbPromise.then(db => {
+        return db.get('inputs', 'name');
+      }).then(value => {
+        if (value) {
+          this.name = value;
+        }
+      });
+    });
+  }
+};
+</script>
