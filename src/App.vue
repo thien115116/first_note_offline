42714fe6e7e35a8f950152785ba626c776331337
thientn <thientn@midota.com>
2023-02-17 09:31:47 +0700

    feat: 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  PWA
+  <div class="container-fluid">
+    <div class="mb-5">
+      <button class="btn btn-primary">
+        Access Offline Mode
+      </button>
+    </div>
+    <SimpleForm />
+    </div>
 </template>
 
 <script>
+import SimpleForm from './components/Form/SimpleForm.vue'
+
 export default {
   name: 'App',
+  components: {
+    SimpleForm
+  }
 }
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import './assets/bootstrap/css/bootstrap.min.css';
+// import './assets/bootstrap/js/bootstrap.min';
 
 createApp(App).mount('#app')
